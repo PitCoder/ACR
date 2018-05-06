105d18713b126b85f77f2c4ca7905e97e6d6df03
PitCoder <overlord.lae@gmail.com>
2018-05-06 11:10:10 -0500

    Changes

--- ChatMulticast/src/graphic/EnviarImagen.java
+++ ChatMulticast/src/graphic/EnviarImagen.java
@@ -1,7 +1,10 @@
 package graphic;
 
 import Object_File.FileObj;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -49,77 +52,105 @@ public class EnviarImagen extends Thread {
         System.out.println(file.getName());
         
         try{
-            BufferedImage bi = ImageIO.read(file);
-            if(bi.getHeight() < 100 || bi.getWidth() <100){
-                System.out.println("Imaga size not valid: minimum size shall be 100 x 00 px");
+            BufferedImage bi = null;
+            Image image;
+
+            image = ImageIO.read(file);
+            bi = ImageIO.read(file);
+            
+            if (image == null) {
+                System.out.println("The file" + file.getName() + "could not be opened , it is not an image");
             }
             else{
-                    try {
-                    MulticastSocket socket = new MulticastSocket(MCAST_PORT);
-                    socket.joinGroup(group); // se configura para escuchar el paquete
+                if(bi.getHeight() < 100 || bi.getWidth() <100){
+                    System.out.println("Imaga size not valid: minimum size shall be 100 x 00 px");
+                }
+                else{
+                        int height = bi.getHeight();
+                        int width = bi.getWidth();
+                        System.out.println("Width: " + width);
+                        System.out.println("Height: " + height);
+                        BufferedImage new_img;
 
-                    //FILE
-                    byte[] bytesDelFile = null;
-                    FileInputStream fileInputStream = null;
-                    bytesDelFile = new byte[(int) file.length()];
+                        width = getOptimalWidth(width);
+                        height = getOptimalHeight(height);
+                        
+                        if(width != -1 && height != -1){
+                            System.out.println("Rendering");
+                            new_img = getScaledInstance(bi, width, height, true);
+                            System.out.println("Rendering ended");
+                            
+                            ImageIO.write(new_img, "jpg", file);
+                            try {
+                                    MulticastSocket socket = new MulticastSocket(MCAST_PORT);
+                                    socket.joinGroup(group); // se configura para escuchar el paquete
 
-                    //leer el archivo sobre el arreglo de bytes
-                    fileInputStream = new FileInputStream(file);
-                    fileInputStream.read(bytesDelFile);
+                                    //FILE
+                                    byte[] bytesDelFile = null;
+                                    FileInputStream fileInputStream = null;
+                                    bytesDelFile = new byte[(int) file.length()];
 
-                    int tbuf = 50000;
-                    int numDatagramas = 0;
+                                    //leer el archivo sobre el arreglo de bytes
+                                    fileInputStream = new FileInputStream(file);
+                                    fileInputStream.read(bytesDelFile);
 
-                    ByteArrayInputStream bais = new ByteArrayInputStream(bytesDelFile);
-                    numDatagramas = (int) bytesDelFile.length / tbuf;
-                    numDatagramas++;    //en el 1ro irà el nombre
+                                    int tbuf = 50000;
+                                    int numDatagramas = 0;
 
-                    if (bytesDelFile.length % tbuf > 0) {                                              // Se debe de enviar un datagrama más.
-                        numDatagramas++;
-                    }
+                                    ByteArrayInputStream bais = new ByteArrayInputStream(bytesDelFile);
+                                    numDatagramas = (int) bytesDelFile.length / tbuf;
+                                    numDatagramas++;    //en el 1ro irà el nombre
 
-                    FileObj obj;
+                                    if (bytesDelFile.length % tbuf > 0) {                                              // Se debe de enviar un datagrama más.
+                                        numDatagramas++;
+                                    }
 
-                    for (int i = 0; i < numDatagramas; i++) {//for datagramas archivo
-                        byte[] tmp = new byte[tbuf];
-                        if (i == 0) {
-                            String nombre = file.getName();
-                            ByteArrayInputStream bais2 = new ByteArrayInputStream(nombre.getBytes());
-                            int n = bais2.read(tmp);
-                            obj = new FileObj(i + 1, numDatagramas, tmp, usuario);
-                            System.out.println("Nombre del archivo: " + new String(obj.getB()));
+                                    FileObj obj;
 
-                        } else {
-                            int n = bais.read(tmp);
-                            obj = new FileObj(i + 1, numDatagramas, tmp, usuario);
+                                    for (int i = 0; i < numDatagramas; i++) {//for datagramas archivo
+                                        byte[] tmp = new byte[tbuf];
+                                        if (i == 0) {
+                                            String nombre = file.getName();
+                                            ByteArrayInputStream bais2 = new ByteArrayInputStream(nombre.getBytes());
+                                            int n = bais2.read(tmp);
+                                            obj = new FileObj(i + 1, numDatagramas, tmp, usuario);
+                                            System.out.println("Nombre del archivo: " + new String(obj.getB()));
+
+                                        } else {
+                                            int n = bais.read(tmp);
+                                            obj = new FileObj(i + 1, numDatagramas, tmp, usuario);
+                                        }
+                                        /* Envío el objeto */
+                                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                                        ObjectOutputStream oos = new ObjectOutputStream(bos);
+                                        oos.writeObject(obj);
+                                        oos.flush();
+
+                                        byte[] buf = bos.toByteArray();
+
+                                        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, MCAST_PORT);
+                                        System.out.println("===========================================");
+                                        System.out.println("packet lenght: " + packet.getLength());
+                                        System.out.println("Paquetes enviados: " + obj.getN());
+                                        System.out.println("Total de paquetes: " + obj.getTotal());
+                                        System.out.println("===========================================");
+                                        socket.send(packet);
+                                        oos.close();
+                                    }//for envio datagramas
+                                    //System.out.println("Enviando: " + msg+"  con un TTL= "+socket.getTimeToLive());
+                                    System.out.println("==Se envió el archivo completo==");
+                                    socket.close();
+                                } 
+                                catch (IOException e) {
+                                    e.printStackTrace();
+                                    System.exit(2);
+                                }
                         }
-                        /* Envío el objeto */
-                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                        ObjectOutputStream oos = new ObjectOutputStream(bos);
-                        oos.writeObject(obj);
-                        oos.flush();
-
-                        byte[] buf = bos.toByteArray();
-
-                        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, MCAST_PORT);
-                        System.out.println("===========================================");
-                        System.out.println("packet lenght: " + packet.getLength());
-                        System.out.println("Paquetes enviados: " + obj.getN());
-                        System.out.println("Total de paquetes: " + obj.getTotal());
-                        System.out.println("===========================================");
-                        socket.send(packet);
-                        oos.close();
-                    }//for envio datagramas
-                    //System.out.println("Enviando: " + msg+"  con un TTL= "+socket.getTimeToLive());
-                    System.out.println("==Se envió el archivo completo==");
-                    socket.close();
-                } 
-                catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(2);
+                        else{
+                            System.out.println("Image too big to display");
+                        }
                 }
             }
-        
         }
         catch (IOException ex) {
             System.out.println("The file" + file.getName() + "could not be opened , an error occurred.");
@@ -131,4 +162,83 @@ public class EnviarImagen extends Thread {
         catch (InterruptedException ie) {
         }
     }//run
+    
+        private BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, boolean higherQuality){
+        int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage) img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } 
+        else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
+    }
+    
+    private int getOptimalWidth(int w){
+        if(w > 2048){
+            return -1;
+        }
+        else{
+            if(w<1920 && w>= 1280){
+                return 550;
+            }
+            else if(w < 1280 && w >720){
+                return 450;
+            }
+            else{
+                return 300;
+            }
+        }
+    }
+    
+    private int getOptimalHeight(int h){
+        if(h > 2048){
+            return -1;
+        }
+        else{
+            if(h<1920 && h>= 1280){
+                return 450;
+            }
+            else if(h < 1280 && h >720){
+                return 350;
+            }
+            else{
+                return 200;
+            }
+        }
+    }
 }
--- ChatMulticast/src/graphic/HTMLpane.java
+++ ChatMulticast/src/graphic/HTMLpane.java
@@ -93,19 +93,7 @@ public class HTMLpane extends JTextPane {
             int width = bi.getWidth();
             System.out.println("Width: " + width);
             System.out.println("Height: " + height);
-            BufferedImage new_img;
-            
-            width = getOptimalWidth(width);
-            height = getOptimalHeight(height);
-            
-            
-            if(width != -1 && height != -1){
-                System.out.println("Rendering");
-                new_img = getScaledInstance(bi, width, height, true);
-                System.out.println("Rendering ended");
-
-
-                ImageIO.write(new_img, "jpg", f1);
+      
                 System.out.println("Width: " + width);
                 System.out.println("Height: " + height);
                 
@@ -116,19 +104,18 @@ public class HTMLpane extends JTextPane {
                     this.setText(text + String.format("<div align=\"left\" > %s </div>", "<img src=\"FILE:///" + f1.getAbsolutePath().replace("\\", "\\\\") + "\" height=\"" + height + "\" width=\"" + width + "\">"));
                 }
             }
-            else{
-                String text = this.getText().split("<body>")[1].split("</body>")[0];
-                if (ismy) {
-                    this.setText(text + String.format("<div align=\"right\" > %s </div>", "<a href = \"" + f1.getAbsolutePath().replace("\\", "\\\\") + "\">" + f1.getName() + "</a>"));
-
-                } else {
-                    this.setText(text + String.format("<div align=\"left\" > %s </div>", "<a href = \"" + f1.getAbsolutePath().replace("\\", "\\\\") + "\">" + f1.getName() + "</a>"));
-                }
-            }
+//            else{
+//                String text = this.getText().split("<body>")[1].split("</body>")[0];
+//                if (ismy) {
+//                    this.setText(text + String.format("<div align=\"right\" > %s </div>", "<a href = \"" + f1.getAbsolutePath().replace("\\", "\\\\") + "\">" + f1.getName() + "</a>"));
+//
+//                } else {
+//                    this.setText(text + String.format("<div align=\"left\" > %s </div>", "<a href = \"" + f1.getAbsolutePath().replace("\\", "\\\\") + "\">" + f1.getName() + "</a>"));
+//                }
+//            }
             
             this.setCaretPosition(this.getDocument().getLength());
         }
-    }
 
     public void seticons(ImageIcon[] img) {
         icons = img;
@@ -140,82 +127,4 @@ public class HTMLpane extends JTextPane {
     }
     ImageIcon[] icons;
     
-    private BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, boolean higherQuality){
-        int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
-        BufferedImage ret = (BufferedImage) img;
-        int w, h;
-        if (higherQuality) {
-            // Use multi-step technique: start with original size, then
-            // scale down in multiple passes with drawImage()
-            // until the target size is reached
-            w = img.getWidth();
-            h = img.getHeight();
-        } 
-        else {
-            // Use one-step technique: scale directly from original
-            // size to target size with a single drawImage() call
-            w = targetWidth;
-            h = targetHeight;
-        }
-
-        do {
-            if (higherQuality && w > targetWidth) {
-                w /= 2;
-                if (w < targetWidth) {
-                    w = targetWidth;
-                }
-            }
-
-            if (higherQuality && h > targetHeight) {
-                h /= 2;
-                if (h < targetHeight) {
-                    h = targetHeight;
-                }
-            }
-
-            BufferedImage tmp = new BufferedImage(w, h, type);
-            Graphics2D g2 = tmp.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-            g2.drawImage(ret, 0, 0, w, h, null);
-            g2.dispose();
-
-            ret = tmp;
-        } while (w != targetWidth || h != targetHeight);
-
-        return ret;
-    }
-    
-    private int getOptimalWidth(int w){
-        if(w > 2048){
-            return -1;
-        }
-        else{
-            if(w<1920 && w>= 1280){
-                return 550;
-            }
-            else if(w < 1280 && w >720){
-                return 450;
-            }
-            else{
-                return 300;
-            }
-        }
-    }
-    
-    private int getOptimalHeight(int h){
-        if(h > 2048){
-            return -1;
-        }
-        else{
-            if(h<1920 && h>= 1280){
-                return 450;
-            }
-            else if(h < 1280 && h >720){
-                return 350;
-            }
-            else{
-                return 200;
-            }
-        }
-    }
 }
